dd9032a983f1f6b329af5604733bd5d281a03762
fsocietyipa <fsocietyipa@github.com>
2020-04-16 23:41:54 +0600

    Changed migration tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username', 60)->unique();
             $table->string('email', 80)->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 

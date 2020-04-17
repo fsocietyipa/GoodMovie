d8b0acd58bfc62b07be22a96bb189baa80956132
fsocietyipa <fsocietyipa@github.com>
2020-04-17 08:47:35 +0600

    Added https on all routes

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,11 @@ Route::get('/details/{id}', 'DetailController@indexById');
 
 Route::get('/register', 'RegistrationController@create');
 Route::post('register', 'RegistrationController@store');
- 
+
 Route::get('/login', 'SessionsController@create');
 Route::post('/login', 'SessionsController@store');
 Route::get('/logout', 'SessionsController@destroy');
+
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}

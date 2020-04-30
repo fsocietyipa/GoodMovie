dfbcf1f8becca30054dfc001a1d3f3253a121ee5
Amirka <ZeroBladeAmir@gmail.com>
2020-05-01 03:55:06 +0600

    Send registration mail

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -1,17 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\User;
-
+use Illuminate\Support\Facades\Log;
+use Mail;
+use Illuminate\Support\Facades\Auth;
 class RegistrationController extends Controller
 {
     public function create()
     {
         return view('registration.create');
     }
-
     public function store()
     {
         $this->validate(request(), [
@@ -20,11 +19,18 @@ class RegistrationController extends Controller
             'password' => 'required|confirmed',
             'is_admin' => 'required'
         ]);
-
         $user = User::create(request(['username', 'email', 'password', 'is_admin']));
-
         auth()->login($user);
+        $this->send();
         return redirect()->to('/');
 //        return view('errorview');
+    }
+    public function send()
+    {
+        Mail::send(['text' => 'mail'], ['name', 'Web dev blog'], function ($message) {
+            $user = Auth::user();
+            $message->to($user["email"], "Hello, {$user['username']}")->subject('Welcome on GoodMovie');
+            $message->from('csse1705@gmail.com', 'Goodmovie');
+        });
     }
 }
---
+++ resources/views/mail.blade.php
@@ -0,0 +1,2 @@
+Thank you for registration on GoodMovie!!!
+Go to: https://goodmovie0.herokuapp.com
